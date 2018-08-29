6111171a324fb485719ddb5c81089d79e3e1dac6
PallaviKoty <39792287+PallaviKoty@users.noreply.github.com>
2018-08-29 17:48:43 +0800

    Add files via upload
    Added msg files for communication between the AGV simulator and ROS2 wrapper

--- door_sensor_pkg_cpp/src/AGV_Publisher.cpp
+++ door_sensor_pkg_cpp/src/AGV_Publisher.cpp
@@ -4,7 +4,8 @@
 */
 #include <chrono>
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/int8.hpp"
+// #include "std_msgs/msg/int8.hpp"
+#include "door_sensor_pkg_cpp/msg/command.hpp"
 
 using namespace std;
 
@@ -19,7 +20,8 @@ public:
   AGVPublisher()
   : Node("agv_publisher"), count_(1)
   {
-    publisher_ = this->create_publisher<std_msgs::msg::Int8>("agv_door_command_topic");
+    // publisher_ = this->create_publisher<std_msgs::msg::Int8>("agv_door_command_topic");
+    publisher_ = this->create_publisher<door_sensor_pkg_cpp::msg::Command>("agv_door_command_topic");
     timer_ = this->create_wall_timer(
       500ms, std::bind(&AGVPublisher::timer_callback, this));
   }
@@ -27,8 +29,9 @@ public:
 private:
   void timer_callback()
   {
-    auto message = std_msgs::msg::Int8();
-    cout << "Hey AGV, do you want the door to be opened? (0-No/1-Yes)" << endl;
+    // auto message = std_msgs::msg::Int8();
+    auto message = door_sensor_pkg_cpp::msg::Command();
+    cout << "Hey AGV here, I want the door to be opened if I send HIGH" << endl;
     cin >> count_;
     if ((count_!=0) && (count_!=1))
     {
@@ -46,12 +49,13 @@ private:
     //   message.data = count_;
     //   count_++;
     // }
-    message.data = count_;
-    RCLCPP_INFO(this->get_logger(), "Publishing: '%d'", message.data)
+    message.signalcommand = count_;
+    RCLCPP_INFO(this->get_logger(), "Publishing: '%d'", message.signalcommand)
     publisher_->publish(message);
   }
   rclcpp::TimerBase::SharedPtr timer_;
-  rclcpp::Publisher<std_msgs::msg::Int8>::SharedPtr publisher_;
+  // rclcpp::Publisher<std_msgs::msg::Int8>::SharedPtr publisher_;
+  rclcpp::Publisher<door_sensor_pkg_cpp::msg::Command>::SharedPtr publisher_;
   size_t count_;
 };
 
--- door_sensor_pkg_cpp/src/RMF_AGV_Subscriber.cpp
+++ door_sensor_pkg_cpp/src/RMF_AGV_Subscriber.cpp
@@ -4,7 +4,8 @@
 */
 
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/int8.hpp"
+// #include "std_msgs/msg/int8.hpp"
+#include "door_sensor_pkg_cpp/msg/command.hpp"
 // #include <wiringPi.h>
 
 using std::placeholders::_1;
@@ -17,29 +18,29 @@ public:
   RMFAGVSubscriber()
   : Node("rmf_agv_subscriber")
   {
-    subscription_ = this->create_subscription<std_msgs::msg::Int8>(
-      "agv_door_command_topic", std::bind(&RMFAGVSubscriber::agv_door_command_topic_callback, this, _1));
+    // subscription_ = this->create_subscription<std_msgs::msg::Int8>("agv_door_command_topic", std::bind(&RMFAGVSubscriber::agv_door_command_topic_callback, this, _1));
+    subscription_ = this->create_subscription<door_sensor_pkg_cpp::msg::Command>("agv_door_command_topic", std::bind(&RMFAGVSubscriber::agv_door_command_topic_callback, this, _1));
   }
 
 private:
-  void agv_door_command_topic_callback(const std_msgs::msg::Int8::SharedPtr msg)
+  // void agv_door_command_topic_callback(const std_msgs::msg::Int8::SharedPtr msg)
+  void agv_door_command_topic_callback(const door_sensor_pkg_cpp::msg::Command::SharedPtr msg)
   {
-    RCLCPP_INFO(this->get_logger(), "Received: '%d'", msg->data);
-    if (msg->data == 1)
+    RCLCPP_INFO(this->get_logger(), "Received: '%d'", msg->signalcommand);
+    if (msg->signalcommand == 1)
     {
-      RCLCPP_INFO(this->get_logger(), "Writing %d to GPIO pins", msg->data);
+      RCLCPP_INFO(this->get_logger(), "Writing %d to GPIO pins", msg->signalcommand);
       // https://bob.cs.sonoma.edu/IntroCompOrg-RPi/sec-cgpio.html
-      // if(wiringPiSetup() == -1)
-      // {
-      //   RCLCPP_INFO(this->get_logger(), "setup wiringPi failed")
-      // }
-      // RCLCPP_INFO(this->get_logger(), "linker LedPin : GPIO %d(wiringPi pin)", GPIO_OUTPIN)
-      // pinMode(GPIO_OUTPIN, OUTPUT);
-      // digitalWrite(LedPin, LOW);
-
+        // if(wiringPiSetup() == -1)
+        // {
+        //   RCLCPP_INFO(this->get_logger(), "setup wiringPi failed")
+        // }
+        // RCLCPP_INFO(this->get_logger(), "linker LedPin : GPIO %d(wiringPi pin)", GPIO_OUTPIN)
+        // pinMode(GPIO_OUTPIN, OUTPUT);
+        // digitalWrite(LedPin, LOW);
     }
   }
-  rclcpp::Subscription<std_msgs::msg::Int8>::SharedPtr subscription_;
+  rclcpp::Subscription<door_sensor_pkg_cpp::msg::Command>::SharedPtr subscription_;
 };
 
 int main(int argc, char * argv[])
