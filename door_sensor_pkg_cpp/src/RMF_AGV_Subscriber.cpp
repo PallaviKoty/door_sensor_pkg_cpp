5042918f22a3f634ff220a6bb4ce0b01327e0a5f
PallaviKoty <39792287+PallaviKoty@users.noreply.github.com>
2018-08-31 15:43:22 +0800

    Add files via upload
    dry contact sensor package with timeout

--- door_sensor_pkg_cpp/src/RMF_AGV_Subscriber.cpp
+++ door_sensor_pkg_cpp/src/RMF_AGV_Subscriber.cpp
@@ -2,15 +2,23 @@
   Created by : Pallavi
   Created on : 28-08-2018
 */
-
+#include <iostream>
 #include "rclcpp/rclcpp.hpp"
+#include "ros2_time/time.hpp"
+#include "rclcpp/timer.hpp"
+#include "rclcpp/executor.hpp"
+#include <sstream>
+#include <chrono>
 // #include "std_msgs/msg/int8.hpp"
 #include "door_sensor_pkg_cpp/msg/command.hpp"
 // #include <wiringPi.h>
+using namespace std;
 
 using std::placeholders::_1;
 
 #define GPIO_OUTPIN 0
+
+int timerFlag= 0;
 
 class RMFAGVSubscriber : public rclcpp::Node
 {
@@ -19,13 +27,33 @@ public:
   : Node("rmf_agv_subscriber")
   {
     // subscription_ = this->create_subscription<std_msgs::msg::Int8>("agv_door_command_topic", std::bind(&RMFAGVSubscriber::agv_door_command_topic_callback, this, _1));
+    RCLCPP_INFO(this->get_logger(), "Value of subscription_ before create_subscription : %d", subscription_);
     subscription_ = this->create_subscription<door_sensor_pkg_cpp::msg::Command>("agv_door_command_topic", std::bind(&RMFAGVSubscriber::agv_door_command_topic_callback, this, _1));
+    RCLCPP_INFO(this->get_logger(), "Value of subscription_ after create_subscription : %d", subscription_);
+    sub_timer_ = this->create_wall_timer(
+      15000ms, std::bind(&RMFAGVSubscriber::sub_timer_callback, this));
   }
 
 private:
+
+  void sub_timer_callback()
+  {
+    if(timerFlag == 0)
+    {
+      RCLCPP_INFO(this->get_logger(), "Timeout already");
+    }
+    else if (timerFlag == 1)
+    {
+      RCLCPP_INFO(this->get_logger(), "Not timeout");
+      timerFlag = 0;
+    }
+
+  }
   // void agv_door_command_topic_callback(const std_msgs::msg::Int8::SharedPtr msg)
   void agv_door_command_topic_callback(const door_sensor_pkg_cpp::msg::Command::SharedPtr msg)
   {
+    timerFlag = 1;
+    // cout << "Received a signal just now : " << exectime.toSec() <<"."<< exectime.toNSec() <<endl;
     RCLCPP_INFO(this->get_logger(), "Received: '%d'", msg->signalcommand);
     if (msg->signalcommand == 1)
     {
@@ -38,15 +66,17 @@ private:
         // RCLCPP_INFO(this->get_logger(), "linker LedPin : GPIO %d(wiringPi pin)", GPIO_OUTPIN)
         // pinMode(GPIO_OUTPIN, OUTPUT);
         // digitalWrite(LedPin, LOW);
+      cout << "Just done with writing. Waiting for next command : "/* << endtime.toSec() << "." << endtime.toNSec() */<< endl;
     }
   }
   rclcpp::Subscription<door_sensor_pkg_cpp::msg::Command>::SharedPtr subscription_;
+  rclcpp::TimerBase::SharedPtr sub_timer_;
 };
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<RMFAGVSubscriber>());
+  rclcpp::spin(std::make_shared<RMFAGVSubscriber>()); 
   rclcpp::shutdown();
   return 0;
 }
