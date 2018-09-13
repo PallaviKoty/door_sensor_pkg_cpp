aa13cfe72d25c63a02312931ff998a5ac0046a39
PallaviKoty <39792287+PallaviKoty@users.noreply.github.com>
2018-09-13 15:26:41 +0800

    Add files via upload

--- door_sensor_pkg_cpp/src/AGV_Publisher.cpp
+++ door_sensor_pkg_cpp/src/AGV_Publisher.cpp
@@ -4,7 +4,6 @@
 */
 #include <chrono>
 #include "rclcpp/rclcpp.hpp"
-// #include "std_msgs/msg/int8.hpp"
 #include "door_sensor_pkg_cpp/msg/command.hpp"
 
 using namespace std;
@@ -18,33 +17,26 @@ class AGVPublisher : public rclcpp::Node
 {
 public:
   AGVPublisher()
-  : Node("agv_publisher"), count_(1)
+  : Node("agv_publisher"), count_(0)
   {
-    // publisher_ = this->create_publisher<std_msgs::msg::Int8>("agv_door_command_topic");
     publisher_ = this->create_publisher<door_sensor_pkg_cpp::msg::Command>("agv_door_command_topic");
-    timer_ = this->create_wall_timer(
-      500ms, std::bind(&AGVPublisher::timer_callback, this));
+    timer_ = this->create_wall_timer(500ms, std::bind(&AGVPublisher::timer_callback, this));
   }
 
 private:
   void timer_callback()
   {
-    // auto message = std_msgs::msg::Int8();
     auto message = door_sensor_pkg_cpp::msg::Command();
-    cout << "Hey AGV here, I want the door to be opened if I send HIGH" << endl;
-    cin >> count_;
-    if ((count_!=1))
+    message.signalcommand=0;
+    if(count_<100) 
     {
-      RCLCPP_INFO(this->get_logger(), "Bad choice : Please enter 1 to open the door")
-      cin >> count_;   
-    }
-
-    message.signalcommand = count_;
+    message.signalcommand =1;
+    count_++;
     RCLCPP_INFO(this->get_logger(), "Publishing: '%d'", message.signalcommand)
     publisher_->publish(message);
+    }
   }
   rclcpp::TimerBase::SharedPtr timer_;
-  // rclcpp::Publisher<std_msgs::msg::Int8>::SharedPtr publisher_;
   rclcpp::Publisher<door_sensor_pkg_cpp::msg::Command>::SharedPtr publisher_;
   size_t count_;
 };
